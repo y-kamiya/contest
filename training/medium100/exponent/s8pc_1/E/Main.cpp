2b814db70ce183fbbb0b540d1fdb774e2aa4a52e
Yuji Kamiya <y.kamiya0@gmail.com>
2022-06-07 09:36:22 +0900

    fix

--- training/medium100/exponent/s8pc_1/E/Main.cpp
+++ training/medium100/exponent/s8pc_1/E/Main.cpp
@@ -35,6 +35,74 @@ void print(vector<T> vec, Tail... t) {
 
 static const ll MOD = 1e9+7;
 
+using u64 = std::uint_fast64_t;
+template<u64 Mod> struct Modint {
+    u64 val;
+    constexpr Modint(const u64 x = 0) noexcept : val(x % Mod) {}
+    constexpr u64 &value() noexcept { return val; }
+    constexpr const u64 &value() const noexcept { return val; }
+    constexpr Modint operator+(const Modint rhs) const noexcept {
+        return Modint(*this) += rhs;
+    }
+    constexpr Modint operator-(const Modint rhs) const noexcept {
+        return Modint(*this) -= rhs;
+    }
+    constexpr Modint operator*(const Modint rhs) const noexcept {
+        return Modint(*this) *= rhs;
+    }
+    constexpr Modint operator/(const Modint rhs) const noexcept {
+        return Modint(*this) /= rhs;
+    }
+    constexpr Modint &operator+=(const Modint rhs) noexcept {
+        val += rhs.val;
+        if (Mod <= val) val -= Mod;
+        return *this;
+    }
+    constexpr Modint &operator-=(const Modint rhs) noexcept {
+        if (val < rhs.val) val += Mod;
+        val -= rhs.val;
+        return *this;
+    }
+    constexpr Modint &operator*=(const Modint rhs) noexcept {
+        val = val * rhs.val % Mod;
+        return *this;
+    }
+    constexpr Modint &operator/=(Modint rhs) noexcept {
+        ll a = rhs.val, b = Mod, u = 1, v = 0;
+        while (b) {
+            ll t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % Mod;
+        if (u < 0) val += Mod;
+        return *this;
+    }
+    constexpr bool operator == (const Modint& rhs) const noexcept {
+        return this->val == rhs.val;
+    }
+    constexpr bool operator != (const Modint& rhs) const noexcept {
+        return this->val != rhs.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Modint<Mod>& x) noexcept {
+        return os << x.value();
+    }
+
+    constexpr Modint modpow(const Modint &mi) noexcept {
+        u64 ret = 1;
+        u64 n = mi.value();
+        while (n > 0) {
+            if (n & 1) ret = ret * val % Mod;
+            val = val * val % Mod;
+            n >>= 1;
+        }
+        val = ret;
+        return *this;
+    }
+};
+
+using mint = Modint<MOD>;
+
 ll modpow(ll a, ll n) {
     ll ret = 1;
     while (n > 0) {
@@ -49,28 +117,32 @@ void _main() {
     int N, Q;
     cin >> N >> Q;
 
-    vector<ll> a(N), b(N, 0), c(Q+2, 0);
-    REP(i, N) cin >> a[i];
+    vector<ll> c(Q+2, 0);
+    vector<mint> a(N), b(N, 0);
+    REP(i, N) {
+        int x; cin >> x;
+        a[i] = x;
+    }
     FOR(i, 1, Q+1) {
         cin >> c[i];
         --c[i];
     }
 
     REP(i, N-1) {
-        b[i+1] = (b[i] + modpow(a[i], a[i+1])) % MOD;
+        b[i+1] = b[i] + a[i].modpow(a[i+1]);
     }
 
     DEBUG(a);
     DEBUG(b);
 
-    ll ans = 0;
+    mint ans = 0;
     REP(i, Q+1) {
         int s = c[i];
         int t = c[i+1];
-        int d = b[t] + MOD - b[s];
-        // if (d < 0) d += MOD;
+        if (s > t) swap(s, t);
+        auto d = b[t] - b[s];
         DEBUG(i, s, t, d);
-        ans = (ans + d) % MOD;
+        ans += d;
     }
     cout << ans << endl;
 }
