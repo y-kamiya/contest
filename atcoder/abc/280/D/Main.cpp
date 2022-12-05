4aa9fe67e01c96c1384bb663b8671e218ab1bb03
y-kamiya <y.kamiya0@gmail.com>
2022-12-05 10:54:22 +0900

    abc280

---
+++ atcoder/abc/280/D/Main.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ostreamFork {
+public:
+    ostream &os1,&os2;
+    ostreamFork(ostream& os1, ostream& os2) : os1(os1), os2(os2) {}
+};
+template <class Data>
+ostreamFork& operator<<(ostreamFork& osf, Data d) {
+    osf.os1 << d; osf.os2 << d; return osf;
+}
+ostreamFork& operator<<(ostreamFork& osf, ostream& (*f)(ostream&)) {
+    osf.os1 << f; osf.os2 << f; return osf;
+}
+template <class ManipData>
+ostreamFork& operator<<(ostreamFork& osf, ostream& (*f)(ostream&, ManipData)) {
+    osf.os1 << f; osf.os2 << f; return osf;
+}
+
+using ll = long long;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n;i>=0;i--)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define FORR(i,a,b) for(int i=(a);i>=(b);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
+#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
+#define PRINT_DOUBLE(n, x) cout << std::fixed << std::setprecision(n) << x << endl;
+
+void print() { cout << endl; }
+template<typename Head, typename... Tail>
+void print(Head h, Tail... t) {
+    cout << h << " "; print(t...);
+}
+template<typename T0, typename T1, typename... Tail>
+void print(pair<T0,T1> p, Tail... t) {
+    cout << "(" << p.first << "," << p.second << ")";
+    print(t...);
+}
+template<typename T, typename... Tail>
+void print(vector<T> vec, Tail... t) {
+    cout << "["; for (const auto &e : vec) { cout << e << ", "; } cout << "] ";
+    print(t...);
+}
+#ifdef _DEBUG
+#define DEBUG(...) print(__VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+
+ofstream file("_output.txt");
+ostreamFork osf(file, cout);
+
+
+void _main() {
+    ll K;
+    cin >> K;
+
+    ll sqrtk = sqrt(K);
+    vector<pair<ll,ll>> ms;
+    FOR(i, 2, sqrtk+1) {
+        ll cnt = 0;
+        while (K % i == 0) {
+            ++cnt;
+            K /= i;
+        }
+        if (cnt != 0) ms.emplace_back(i, cnt);
+    }
+    if (K != 1) ms.emplace_back(K, 1);
+
+
+    ll ans = 0;
+    for (auto [m, cnt] : ms) {
+        DEBUG("sss", m, cnt);
+        ll s = 1;
+        ll c = cnt;
+        FOR(i, 1, cnt+1) {
+            s = m * i;
+            --c;
+            int j = i;
+            while (j % m == 0) {
+                --c;
+                j /= m;
+            }
+            DEBUG(i, c);
+            if (c <= 0) break;
+        }
+        DEBUG(m, c, s);
+        ans = max(ans, s);
+    }
+
+    osf << ans << endl;
+
+
+}
+
+int main() {
+    _main();
+    return 0;
+}
+
